9116fe0c54be4e04f217f9534a54664e1162d9db
Joni Laurila <joni.laurila@gofore.com>
2017-05-03 10:30:12 +0300

    Updating create-new-presentation script to fix title of index.html

--- create-new-presentation.sh
+++ create-new-presentation.sh
@@ -2,11 +2,13 @@
 
 if [ -z "$1" ]
   then
-    echo "No argument supplied"
+    echo "No argument supplied, give name of presentation"
     exit 1
 fi
 
 mkdir koulutukset/$1
 cd koulutukset/$1
 cp ../template-index.html ./index.html
+sed -i -e "s/Koulutus/$1/g" index.html
+rm index.html-e
 touch presentation.md
